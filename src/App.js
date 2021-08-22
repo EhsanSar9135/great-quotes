e2b779bb1ab8e8575a17042a4301ada60d695c6a
ehsan <ehsansaraee6570@yahoo.com>
2021-08-22 16:59:23 +0430

    Adding Not Found page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import NewQuote from "./routes/NewQuote";
+import NotFound from "./routes/NotFound";
 import QuoteDetail from "./routes/QuoteDetail";
 import Quotes from "./routes/Quotes";
 
@@ -12,6 +13,7 @@ function App() {
             <Route path="/quotes" component={Quotes} exact />
             <Route path="/quotes/:quoteId" component={QuoteDetail} />
             <Route path="/new-quote" component={NewQuote} />
+            <Route path="*" component={NotFound} />
          </Switch>
       </Layout>
    );
